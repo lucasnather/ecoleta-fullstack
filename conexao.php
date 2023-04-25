72bb337328949f4bfefb1806586acff30ed63654
lucasnather <natherlucas@gmail.com>
2023-04-25 17:33:14 -0400

    conexao heroku

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php 
 
-$localhost = "localhost";
-$username = "root";
-$password = "";
-$dbName = "coleta";
+$localhost = "us-cdbr-east-06.cleardb.net";
+$username = "bdbb6d4e646d10";
+$password = "6692f3d1";
+$dbName = "heroku_0b64fa25ddf5652";
 
 $conexao = mysqli_connect($localhost, $username, $password, $dbName);
 
